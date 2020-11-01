1aa1ecfc5e05a27f4b8367a3022905bf3eef370e
Ewan <em702@ip-172-31-19-5.eu-west-2.compute.internal>
2020-11-01 12:19:57 +0000

    Changed so that the correct path is in the jlink command file

--- build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c.obj: \
- /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c \
- /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h \
- /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h
-
-/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h:
-
-/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h:
--- build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_printf.c.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_printf.c.obj: \
- /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_printf.c \
- /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h \
- /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h
-
-/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h:
-
-/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h:
