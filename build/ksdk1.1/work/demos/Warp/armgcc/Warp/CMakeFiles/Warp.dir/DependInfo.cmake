12d964987ef48b4ed1ea30bcab8da7a867d19f66
Ewan <em702@ip-172-31-19-5.eu-west-2.compute.internal>
2020-11-01 12:53:00 +0000

    Adding back the 'blocked files' to my github account

---
+++ build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/platform/startup/MKL03Z4/gcc/startup_MKL03Z4.S" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/platform/startup/MKL03Z4/gcc/startup_MKL03Z4.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../../../../platform/utilities/inc"
+  "../../../../platform/osa/inc"
+  "../../../../platform/CMSIS/Include"
+  "../../../../platform/CMSIS/Include/device"
+  "../../../../platform/startup/MKL03Z4"
+  "../../../../platform/hal/inc"
+  "../../../../platform/drivers/inc"
+  "../../../../platform/system/inc"
+  "../../../../boards/Warp"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/boards/Warp/gpio_pins.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/boards/Warp/gpio_pins.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_printf.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_printf.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devAMG8834.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devAMG8834.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devBME680.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devBME680.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devBMX055.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devBMX055.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devCCS811.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devCCS811.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devHDC1000.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devHDC1000.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devISL23415.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devISL23415.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devL3GD20H.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devL3GD20H.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devMAG3110.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devMAG3110.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devMMA8451Q.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devMMA8451Q.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-boot.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-boot.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-powermodes.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/warp-kl03-ksdk1.1-powermodes.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/i2c/fsl_i2c_irq.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/i2c/fsl_i2c_irq.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_irq.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_irq.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/platform/startup/MKL03Z4/system_MKL03Z4.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/platform/startup/MKL03Z4/system_MKL03Z4.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/platform/startup/startup.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/platform/startup/startup.c.obj"
+  "/students/em702/Warp-firmware/build/ksdk1.1/work/platform/utilities/src/fsl_misc_utilities.c" "/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/platform/utilities/src/fsl_misc_utilities.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../../../platform/utilities/inc"
+  "../../../../platform/osa/inc"
+  "../../../../platform/CMSIS/Include"
+  "../../../../platform/CMSIS/Include/device"
+  "../../../../platform/startup/MKL03Z4"
+  "../../../../platform/hal/inc"
+  "../../../../platform/drivers/inc"
+  "../../../../platform/system/inc"
+  "../../../../boards/Warp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c.d
@@ -0,0 +1,8 @@
+CMakeFiles/Warp.dir/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c.obj: \
+ /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.c \
+ /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h \
+ /students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h
+
+/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h:
+
+/students/em702/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h:
